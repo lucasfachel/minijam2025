2f295e85eacf639cb7bc220fc1cbbe240c79ceed
Leonardo <leonardo.moreira@exporj.com.br>
2025-06-23 01:22:50 -0300

    aaaa

--- dualidade-jam/scripts/shop.gd
+++ dualidade-jam/scripts/shop.gd
@@ -8,6 +8,10 @@ var ativo = false
 #inventario loja$"../Camera2D/ShopWindow"
 @onready var pernas = $"../Camera2D/ShopWindow/Pernas"
 @onready var olhos = $"../Camera2D/ShopWindow/Olhos"
+@onready var jump = $"../Camera2D/ShopWindow/Olhos"
+@onready var arm = $"../Camera2D/ShopWindow/Olhos"
+@onready var flip = $"../Camera2D/ShopWindow/Olhos"
+
 @onready var player = $"../CharacterBody2D"
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +19,9 @@ func _ready() -> void:
 	#para cada item da loja faça isso
 	pernas.pressed.connect(Callable(self, "compra").bind(1))
 	olhos.pressed.connect(Callable(self, "compra").bind(2))
+	jump.pressed.connect(Callable(self, "compra").bind(3))
+	arm.pressed.connect(Callable(self, "compra").bind(4))
+	flip.pressed.connect(Callable(self, "compra").bind(5))
 	
 	var player_node = player
 	label.visible = false
@@ -55,4 +62,15 @@ func compra(item):
 			master.unlockEyes()
 			olhos.get_child(0).visible = false
 			olhos.get_child(1).visible = true
-		3: pass
+		3: 
+			master.unlockJump()
+			jump.get_child(0).visible = false
+			jump.get_child(1).visible = true
+		4: 
+			master.unlockArm()
+			arm.get_child(0).visible = false
+			arm.get_child(1).visible = true
+		5: 
+			master.unlockFlip()
+			flip.get_child(0).visible = false
+			flip.get_child(1).visible = true
